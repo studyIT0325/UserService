f6328006430307278254fe40b7eae34671b157e1
studyIT0325 <studyIT0325@gmail.com>
2024-11-06 17:15:18 +0900

    userservice

--- src/main/java/com/example/user/UserController.java
+++ src/main/java/com/example/user/UserController.java
@@ -2,8 +2,6 @@ package com.example.user;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,8 +18,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class UserController {
 	@Autowired
 	private UserRepository userRepository;
-	
-	private final static Logger log = LoggerFactory.getLogger(UserController.class);
 	
 	@GetMapping("")
 	public String getUsers(Model model) {
